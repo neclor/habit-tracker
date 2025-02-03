6bf33ce2248fb943a6b085d792570fb5e22e2d85
neclor <neclordev@gmail.com>
2025-02-03 10:06:51 +0100

    a

--- app/src/main/java/site/neclor/habittracker/MainActivity.kt
+++ app/src/main/java/site/neclor/habittracker/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,17 +19,30 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContent {
-			HabitTrackerTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
-						name = "Android",
-						modifier = Modifier.padding(innerPadding)
-					)
-				}
-			}
+			Content()
 		}
 	}
 }
+
+@Preview(showBackground = true)
+@Composable
+fun Content() {
+	MaterialTheme {  }
+	HabitTrackerTheme {
+		Scaffold(
+			modifier = Modifier.fillMaxSize()
+		) { innerPadding ->
+			Greeting(
+				name = "Android",
+				modifier = Modifier.padding(innerPadding)
+			)
+		}
+	}
+
+
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- app/src/main/java/site/neclor/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/site/neclor/habittracker/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package site.neclor.habittracker.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +13,19 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
 	primary = Purple80,
 	secondary = PurpleGrey80,
-	tertiary = Pink80
+	tertiary = Pink80,
+
+	background = DarkBackground,
+	surface = DarkBackground2,
+	onPrimary = DarkAccent,
 )
 
 private val LightColorScheme = lightColorScheme(
 	primary = Purple40,
 	secondary = PurpleGrey40,
-	tertiary = Pink40
+	tertiary = Pink40,
+
+	background = LightBackground,
 
 	/* Other default colors to override
     background = Color(0xFFFFFBFE),
