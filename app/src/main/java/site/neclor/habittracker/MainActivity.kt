e5fd346cae8a6a26383f795329f0985ebc55415a
neclor <neclordev@gmail.com>
2025-02-06 18:46:19 +0100

    b

--- app/src/main/java/site/neclor/habittracker/MainActivity.kt
+++ app/src/main/java/site/neclor/habittracker/MainActivity.kt
@@ -6,39 +6,56 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import site.neclor.habittracker.ui.theme.HabitTrackerTheme
 
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		enableEdgeToEdge()
-		setContent {
-			Content()
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun Content() {
-	MaterialTheme {  }
-	HabitTrackerTheme {
-		Scaffold(
-			modifier = Modifier.fillMaxSize()
-		) { innerPadding ->
-			Greeting(
-				name = "Android",
-				modifier = Modifier.padding(innerPadding)
-			)
-		}
-	}
+    MaterialTheme { }
+    HabitTrackerTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                @OptIn(ExperimentalMaterial3Api::class)
+                TopAppBar(
+                    title = {
+                        Text("Habit Tracker")
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
 
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    Text("bebra")
 
 
 }
@@ -46,16 +63,8 @@ fun Content() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	HabitTrackerTheme {
-		Greeting("Android")
-	}
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
